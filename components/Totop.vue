58115127b98d5e5605227623557ff73b24f98b96
umarmuhd <umarfarouqft@gmail.com>
2022-02-22 06:54:07 +0100

    changes

--- components/Totop.vue
+++ components/Totop.vue
@@ -1,48 +1,51 @@
 <template>
-  <button v-show="show" class="btn btn-primary br-curve totop btn-lg shadow" @click="scrollToTop()">
-      <i class="fas fa-angle-up"></i>
+  <button
+    v-show="show"
+    class="btn-primary totop btn-lg shadow"
+    style="border-radius: 24px"
+    @click="scrollToTop()"
+  >
+    <i class="fas fa-angle-up"></i>
   </button>
 </template>
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-    data(){
-        return {
-            show: false
-        }
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    scrollToTop() {
+      if (process.browser) {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
     },
-    methods: {
-        scrollToTop(){
-            if(process.browser){
-                window.scrollTo({
-                    top: 0,
-                    behavior: 'smooth'
-                })
-            }
-        },
-        onScroll(){
-            if(process.browser){
-               if(window.scrollY > 2000){
-                   this.show = true
-               }else{
-                   this.show =false;
-               }
-            }
-
-            // console.log(this.show, window.scrollY)
+    onScroll() {
+      if (process.browser) {
+        if (window.scrollY > 2000) {
+          this.show = true;
+        } else {
+          this.show = false;
         }
+      }
     },
-    mounted(){
-        window.addEventListener('scroll', this.onScroll)
-    }
-})
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+});
 </script>
 <style scoped>
-    .totop{
-        position: fixed;
-        z-index: 3000;
-        bottom: 10%;
-        right: 5%;
-        transition: all 0.5s;
-    }
+.totop {
+  position: fixed;
+  z-index: 3000;
+  bottom: 10%;
+  right: 5%;
+  transition: all 0.5s;
+}
 </style>
